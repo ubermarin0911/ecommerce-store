7b50ece5715786a901df1047e7e0cf88a0911f5a
ubermarin <ubermarinaa@unimagdalena.edu.co>
2021-01-26 17:39:46 -0500

    Se agregó funcionalidad para filtrar los productos por tipo y marca en el cliente

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { IPagination } from '../shared/models/pagination';
 import { IBrand } from '../shared/models/brand';
 import { IType } from '../shared/models/productType';
- 
+import {map} from 'rxjs/operators'; 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,8 +14,24 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(){
-    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  getProducts(brandId?: number, typeId?: number){
+
+    let params = new HttpParams();
+
+    if(brandId){
+      params = params.append('brandId', brandId.toString());
+    }
+
+    if(typeId){
+      params = params.append('typeId', typeId.toString());
+    }
+
+    return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
   }
 
   getBrands(){
