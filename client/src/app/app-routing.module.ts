32acf93abfc7e3f3747aff52c8901e6507d6d8f0
ubermarin <ubermarinaa@unimagdalena.edu.co>
2021-01-27 15:00:18 -0500

    Se agregó lazy loading

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ShopComponent } from './shop/shop.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'shop', component: ShopComponent},
-  {path: 'shop/:id', component: ProductDetailsComponent},
+  {path: '', component: HomeComponent}, 
+  {path: 'shop', loadChildren: './shop/shop.module#ShopModule'},
   {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
 
