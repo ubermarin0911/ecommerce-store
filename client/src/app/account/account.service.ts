d28b98646b685d6457938e02da76c713a15fba78
ubermarin <ubermarinaa@unimagdalena.edu.co>
2021-02-09 09:43:51 -0500

    Se agregó auth guard y se hizo validación asíncrona de correo electrónico

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of, ReplaySubject } from 'rxjs';
 import { IUser } from '../shared/models/user';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
@@ -11,16 +11,17 @@ import { Router } from '@angular/router';
 })
 export class AccountService {
   baseUrl = environment.apiUrl;
-  private currentUserSource = new BehaviorSubject<IUser>(null);
+  private currentUserSource = new ReplaySubject<IUser>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  getCurrentUserValue(){
-    return this.currentUserSource.value;
-  }
-
   loadCurrentUser(token: string){
+    if(token === null){
+        this.currentUserSource.next(null);
+        return of(null);
+    }
+
     let headers = new HttpHeaders();
      headers = headers.set('Authorization', `Bearer ${token}`);
 
@@ -50,6 +51,7 @@ export class AccountService {
       map((user: IUser) => {
         if(user){
           localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
         }
       })
     )
