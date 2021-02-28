14147b39c4cc640bf7e5c164f59ace18d4ff5112
ubermarin <ubermarinaa@unimagdalena.edu.co>
2021-02-28 16:42:17 -0500

    Se cambiaron colores de bootstrap

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -4,10 +4,7 @@ import { BasketService } from '../../basket/basket.service';
 import { CheckoutService } from '../checkout.service';
 import { ToastrService } from 'ngx-toastr';
 import { IBasket } from 'src/app/shared/models/basket';
-import { IOrder } from '../../shared/models/order';
-import { NavigationExtras, Router } from '@angular/router';
-
-declare var Stripe;
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout-payment',
@@ -16,19 +13,8 @@ declare var Stripe;
 })
 export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
   @Input() checkoutForm: FormGroup;
-  @ViewChild('cardNumber', {static: true}) cardNumberElement: ElementRef;
-  @ViewChild('cardExpiry', {static: true}) cardExpiryElement: ElementRef;
-  @ViewChild('cardCvc', {static: true}) cardCvcElement: ElementRef;
-  stripe: any;
-  cardNumber: any;
-  cardExpiry: any;
-  cardCvc: any;
-  cardErrors: any;
-  cardHandler = this.onChange.bind(this);
+ 
   loading = false;
-  cardNumberValid = false;
-  cardExpiryValid = false;
-  cardCvcValid = false;
 
   constructor(
     private basketService: BasketService, 
@@ -37,81 +23,15 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
     private router: Router) { }
 
   ngOnDestroy(): void {
-    // this.cardNumber.destroy();
-    // this.cardExpiry.destroy();
-    // this.cardCvc.destroy();
-  }
-
-  onChange(event){
-    
-    if(event.error){
-      this.cardErrors = event.error.message;
-    }else{
-      this.cardErrors = null;
-    }
-
-    switch(event.elementType){
-      case 'cardNumber':
-        this.cardNumberValid = event.complete;
-        break;
-      case 'cardExpiry':
-        this.cardExpiryValid = event.complete;
-        break;
-      case 'cardCvc':
-        this.cardCvcValid = event.complete;
-        break;
-    }
   }
 
   ngAfterViewInit(): void {
-    this.stripe = Stripe('pk_test_2PZ84pFKu2MddUgGDG521v9m00SlLWySIR');
-    // const elements = this.stripe.element();
-
-    // this.cardNumber = elements.create('cardNumber');
-    // this.cardNumber.mount(this.cardNumberElement.nativeElement);
-    // this.cardNumber.addEventListener('change', this.cardHandler);
-
-    // this.cardExpiry = elements.create('cardExpiry');
-    // this.cardExpiry.mount(this.cardExpiryElement.nativeElement);
-    // this.cardExpiry.addEventListener('change', this.cardHandler);
-
-    // this.cardCvc = elements.create('cardCvc');
-    // this.cardCvc.mount(this.cardCvcElement.nativeElement);
-    // this.cardCvc.addEventListener('change', this.cardHandler);
   }
 
   async submitOrder(){
     this.loading = true;
     const basket = this.basketService.getCurrentBasketValue();
-
-    try {
-      const createdOrder = await this.createOrder(basket);
-      const paymentResult = await this.confirmPaymentWithStripe(basket);
-  
-      if(paymentResult.paymentIntent){
-        this.basketService.deleteBasket(basket);
-        const navigationExtras: NavigationExtras = {state: createdOrder};
-        this.router.navigate(['checkout/success'], navigationExtras);
-      }else{
-        this.toastr.error(paymentResult.error.message);
-      }
-      this.loading = false;
-    } catch (error) {
-      console.log(error);
-      this.loading = false;
-    }
   } 
-
-  private async confirmPaymentWithStripe(basket) {
-   return this.stripe.confirmCardPayment(basket.clientSecret, {
-        payment_method: {
-          card: this.cardNumber,
-          billing_details: {
-            name: this.checkoutForm.get('paymentForm').get('nameOnCard').value
-          }
-        }
-      });
-  }
 
   private async createOrder(basket: IBasket) {
     const orderToCreate = this.getOrderToCreate(basket);
@@ -126,5 +46,4 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
        shipToAddress: this.checkoutForm.get('addressForm').value
     };
   }
-
 }
